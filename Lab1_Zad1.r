85d52f56d3a073385535efbb40cb13830270b954
Patryk Bajgot <45692984+TorQuare@users.noreply.github.com>
2023-10-28 16:16:08 +0200

    Update Lab1_Zad1.r
    Poprawka literówek i wynik.ga

--- Lab1_Zad1.r
+++ Lab1_Zad1.r
@@ -4,27 +4,26 @@ t = function(c, a)
 {
   (c*a + 2*c + 3*a)*sin(2*a+c)
 }
-t = seq(-4,6,by=0.08)
-a = seq(3, 9, by=0.09)
-z = outer(c, a, t)
+c <- seq(-4,6,by=0.08)
+a <- seq(3, 9, by=0.09)
+z <- outer(c, a, t)
 persp(c, a, z, theta = 30, phi = 30,
-      expand = 0.5, col = 8)
+      expand = 0.5, col = 7)
 image(c, a, z, col = terrain.colors(12))
-contour(c, a, z, add = T, col = "magenta12", nlevels = 12)
+contour(c, a, z, add = T, col = "gray50", nlevels = 12)
 
 fm = function(obj){
-  curve(fp, -7,7,main=paste("iteration =", obj@iter))
-  points(obj@population, obj@fitness, pch=20, col="red")
+  contour(c, a, z, col = "darkgray", nlevels = 12,
+         main = paste("iteration =", obj@iter))
+  points(obj@population, pch=20)
 }
 
 maxIter=500
 rPopulacji=20
 
-wynik.ga <- ga(type = "real-valued", fitness = fp, 
-               lower = -7, upper = 7, popSize = rPopulacji,
-               pcrossover = 0.9, pmutation=0.05,
-               elitism = rPopulacji*0.1, monitor = fm,
-               maxiter = maxIter, run = maxIter/10, 
-               keepBest = TRUE, seed = 10)
+wynik.ga <-ga(type="real-valued",lower=c(-4, 3),upper=c(6, 9),
+           fitness=function(x) t(x[1],x[2]),monitor=fm,
+           popSize=20,pcrossover=0.85,pmutation=0.05,
+           elitism=5,maxiter=60,seed=10)
 summary(wynik.ga)
 plot(wynik.ga)
