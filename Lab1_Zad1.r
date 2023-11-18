9857419475f0e3e705415711e4d583192737864d
Patryk Bajgot <45692984+TorQuare@users.noreply.github.com>
2023-11-18 22:19:05 +0100

    Update Lab1_Zad1.r

--- Lab1_Zad1.r
+++ Lab1_Zad1.r
@@ -1,29 +1,30 @@
 library(GA)
 
-t = function(c, a)
-{
-  (c*a + 2*c + 3*a)*sin(2*a+c)
+fitness_func <- function(x1, x2) {
+  return((2*x1+x2^2)-4)
 }
-c <- seq(-4,6,by=0.08)
-a <- seq(3, 9, by=0.09)
-z <- outer(c, a, t)
-persp(c, a, z, theta = 30, phi = 30,
-      expand = 0.5, col = 7)
-image(c, a, z, col = terrain.colors(12))
-contour(c, a, z, add = T, col = "gray50", nlevels = 12)
 
-fm = function(obj){
-  contour(c, a, z, col = "darkgray", nlevels = 12,
-         main = paste("iteration =", obj@iter))
+min_x1 = -4
+min_x2 = -12
+max_x1 = 2
+max_x2 = 8.5
+
+x1 <- seq(min_x1, max_x1, by=0.8)
+x2 <- seq(min_x2, max_x2, by=0.5)
+z <-outer(x1, x2, fitness_func)
+persp(x1, x2, z, theta=30, phi=30, expand=0.5, col=7)
+image(x1, x2, z, col=terrain.colors(50))
+contour(x1, x2, z, add=T, col="darkblue", nlevels=50)
+
+monitor_func <- function(obj) {
+  contour(x1, x2, z, col="magenta", nlevels=50,
+          main=paste("iteration = ", obj@iter))
   points(obj@population, pch=20)
 }
 
-maxIter=500
-rPopulacji=20
-
-wynik.ga <-ga(type="real-valued",lower=c(-4, 3),upper=c(6, 9),
-           fitness=function(x) t(x[1],x[2]),monitor=fm,
+wyniki<-ga(type="real-valued",lower=c(min_x1, min_x2),upper=c(max_x1, max_x2),
+           fitness=function(x) fitness_func(x[1],x[2]),monitor=monitor_func,
            popSize=20,pcrossover=0.85,pmutation=0.05,
-           elitism=5,maxiter=60,seed=10)
-summary(wynik.ga)
-plot(wynik.ga)
+           elitism=5,maxiter=120,seed=10, keepBest = TRUE)
+summary(wyniki)
+plot(wyniki)
